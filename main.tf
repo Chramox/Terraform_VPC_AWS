e4fd1beca17fd8b4cb5de10285a42a65a0cee135
Juan Ibarra <2994775760101@ingenieria.usac.edu.gt>
2022-05-16 18:25:29 -0600

    [ADD] main

--- main.tf
+++ main.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 3.27"
     }
   }
+  required_version = ">= 0.14.9"
 }
 
 variable "aws_region" {
@@ -17,8 +18,9 @@ variable "base_cidr_block" {
 }
 
 variable "availability_zones" {
+  type = list(string)
   description = "A list of availability zones in which to create subnets"
-  type = list("us-west-2a","us-west-2b","us-west-2c")
+  default = ["us-west-2a","us-west-2b","us-west-2c"]
 }
 
 provider "aws" {
@@ -27,26 +29,27 @@ provider "aws" {
 
 resource "aws_vpc" "jmi_terraform_vpc" {
   cidr_block = var.base_cidr_block
+  tags = {
+    Name = "vpc_jmi_tf"
+  }
 }
 
 resource "aws_internet_gateway" "ig_jmi" {
   vpc_id = aws_vpc.jmi_terraform_vpc.id
   tags = {
-    Name        = "internet_gateway_jmi"
+    Name        = "internet_gateway_jmi_tf"
   }
 }
 
 resource "aws_eip" "nat_eip" {
-  vpc        = true
-  depends_on = [aws_internet_gateway.id]
+  depends_on = [aws_internet_gateway.ig_jmi]
 }
 
 resource "aws_nat_gateway" "nat_jmi" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = element(aws_subnet.public_subnet_jmi.*.id, 0)
-
   tags = {
-    Name        = "nat_jmi"
+    Name        = "nat_jmi_tf"
   }
 }
 
@@ -58,9 +61,9 @@ resource "aws_subnet" "public_subnet_jmi" {
 
   vpc_id = aws_vpc.jmi_terraform_vpc.id
 
-  cidr_block = cidrsubnet(aws_vpc.jmi_terraform_vpc.cidr_block, 4, count.index+1)
+  cidr_block = cidrsubnet(aws_vpc.jmi_terraform_vpc.cidr_block, 4, count.index+4)
   tags = {
-    Name = "jmi-public-subnet-${count.index}"
+    Name = "jmi-public-subnet-${count.index}-tf"
   }
 }
 
@@ -74,7 +77,7 @@ resource "aws_subnet" "private_subnet_jmi" {
 
   cidr_block = cidrsubnet(aws_vpc.jmi_terraform_vpc.cidr_block, 4, count.index+1)
   tags = {
-    Name = "jmi-private-subnet-${count.index}"
+    Name = "jmi-private-subnet-${count.index}-tf"
   }
 }
 
@@ -98,7 +101,20 @@ resource "aws_route_table" "public_rt" {
 
 # Route for Internet Gateway
 resource "aws_route" "public_internet_gateway" {
-  route_table_id         = aws_route_table.public.id
+  route_table_id         = aws_route_table.public_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.ig.id
+  gateway_id             = aws_internet_gateway.ig_jmi.id
+}
+
+# Route table associations
+resource "aws_route_table_association" "public" {
+  count          = length(var.availability_zones)
+  subnet_id      = element(aws_subnet.public_subnet_jmi.*.id, count.index)
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.availability_zones)
+  subnet_id      = element(aws_subnet.private_subnet_jmi.*.id, count.index)
+  route_table_id = aws_route_table.private_rt.id
 }
