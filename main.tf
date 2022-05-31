893cadf1e4ec52db691ce491b224cf607300b670
Juan Ibarra <2994775760101@ingenieria.usac.edu.gt>
2022-05-30 20:45:56 -0600

    [ADD] Homework 3

--- main.tf
+++ main.tf
@@ -8,12 +8,24 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
+locals {
+  DateTime = formatdate("DD MMM YYYY - HH:mm AA ZZZ", timestamp())
+}
+
 variable "aws_region" {
     default = "us-west-2"
 }
 
 provider "aws" {
   region = var.aws_region
+
+  default_tags {
+    tags = {
+      CreationDateTime = local.DateTime
+      Owner       = "JuanIbarra"
+    }
+  }
+   
 }
 
 
